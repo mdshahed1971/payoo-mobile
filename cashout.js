1b1eca25ffc8515aa5b688fa19b6f19336a78f6d
shahed <mdshahed19711@gmail.com>
2025-10-26 23:56:58 +0600

    added payoo mobile

--- cashout.js
+++ cashout.js
@@ -1,14 +1,29 @@
 document.getElementById('withdraw-btn').addEventListener('click', function(e){
     e.preventDefault();
+    const cashoutAccount = document.getElementById('cashout-account').value;
     const withdrawAmount = getInputValueByIdFloat('withdraw-amount');
     const withdrawPin = getInputValueByIdInt('withdraw-pin');
     const mainBalance = getInnerTextValueById('main-balance')
-   
+    
+   if(withdrawAmount> mainBalance){
+    alert('Insuffient balance to withdraw');
+    return;
+   }
+
     if(withdrawPin===1234){
        const newWithdraw = mainBalance - withdrawAmount;
        setInnerTextValueById('main-balance', newWithdraw);
        clearTextValueById('withdraw-amount');
        clearTextValueById('withdraw-pin');
+
+        const transitionContainer = document.getElementById('Transition-History');
+        const div = document.createElement('div');
+        div.classList.add('bg-indigo-500', 'text-white', 'rounded-md', 'p-5', 'mb-5','mt-5')
+        div.innerHTML = `
+        <h2>Cashout ${withdrawAmount} TAKA:</h2>
+        <h3>Account number is: ${cashoutAccount}</h3>
+    `
+        transitionContainer.appendChild(div);
     }
     else{
         alert('Invalid pin number')
--- main-balance.js
+++ main-balance.js
@@ -1,15 +1,33 @@
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
+    const accountNumber = document.getElementById('account-number').value;
     const amount = getInputValueByIdFloat('add-amount');
     const pin = getInputValueByIdInt('add-pin');
     const mainBalance = getInnerTextValueById('main-balance');
 
+    if(amount<0){
+        alert('Insuffient balance to add');
+        return;
+    }
    
     if(pin===1234){
     const newAmount = amount + mainBalance;
     setInnerTextValueById('main-balance',newAmount);
     clearTextValueById('add-amount');
     clearTextValueById('add-pin');
+
+    const transitionContainer = document.getElementById('Transition-History');
+    const banks = document.getElementById('all-bank').value;
+    const div = document.createElement('div');
+    div.classList.add('bg-indigo-500', 'text-white', 'rounded-md', 'p-5', 'mb-5','mt-5')
+    div.innerHTML = `
+    <h2>Added ${amount} TAKA:</h2>
+    <h2>From ${banks}</h2>
+     <h3>Account number is: ${accountNumber}</h3>
+    `
+   transitionContainer.appendChild(div);
+
+
     }
     else{
         alert('Invalid pin number')
